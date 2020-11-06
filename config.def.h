e1b8f61bb901da2ddd417a48d45ce5ce926d4130
Charlie Carolan <charliecarolan1999@gmail.com>
2020-11-06 12:38:25 +0000

    Updated color scheme and height

--- config.def.h
+++ config.def.h
@@ -4,12 +4,12 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"undefined medium:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeNorm] = { "#6930c3", "#80ffdb" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
