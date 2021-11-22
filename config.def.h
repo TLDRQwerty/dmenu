9419e67d5673a3c55ac30204c15f6290481d797e
Charlie Carolan <charliecarolan1999@gmail.com>
2021-11-22 01:40:53 +0000

    config stuff

--- config.def.h
+++ config.def.h
@@ -3,14 +3,18 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = { "undefined medium:size=10" };
+static const char col_gray1[]       = "#21143a";
+static const char col_gray2[]       = "#ae00fa";
+static const char col_gray3[]       = "#37ffff";
+static const char col_gray4[]       = "#33ff9c";
+static const char col_cyan[]        = "#e7008b";
+
+static const char *prompt      = ">";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { col_gray3, col_gray1 },
+	[SchemeSel] = { col_gray4, col_cyan },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
