3ea3f8d061e4694a388c3314adc162a27ce28b07
Charlie Carolan <charliecarolan1999@gmail.com>
2021-11-22 01:44:00 +0000

    feat(config): color scheme

--- config.def.h
+++ config.def.h
@@ -2,16 +2,20 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"undefined medium:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = { "undefined medium:size=10" };
+static const char col_gray1[]       = "#21143a";
+static const char col_gray2[]       = "#ae00fa";
+static const char col_gray3[]       = "#37ffff";
+static const char col_gray4[]       = "#33ff9c";
+static const char col_cyan[]        = "#e7008b";
+
+static const char *prompt      = ">";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#6930c3", "#80ffdb" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { col_gray3, col_gray1 },
+	[SchemeSel] = { col_gray4, col_cyan },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
